4144081ea3306edda96c2b45ebaf1ec9b0c031f8
Raven <ravenshooper@gmail.com>
2017-11-28 10:31:26 -0500

    initial changes
    works with devise user
    Has comments
    Has Articles

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  resources :articles do
+    resources :comments
+  end
+  
+  get 'welcome/index'
+  root to: 'welcome#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
